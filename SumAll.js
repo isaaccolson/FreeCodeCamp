3b6aa1d32206c32cb3796d364bf1097e2621cdd5
Isaac Coslon <isaac.colson@gmail.com>
2019-10-17 11:38:10 -0400

    SumAll

---
+++ SumAll.js
@@ -0,0 +1,25 @@
+function sumAll(arr) {
+
+var sumOfArray = 0;
+var lowNumber, highNumber;
+
+if(arr[0] <= arr[1]){
+  lowNumber = arr[0];
+  highNumber = arr[1];
+}else{
+  lowNumber = arr[1];
+  highNumber = arr[0];
+}
+
+
+for(let i = lowNumber; i <= highNumber; i++){
+  sumOfArray += i;
+}
+
+console.log(sumOfArray);
+
+  return sumOfArray;
+
+}
+
+sumAll([1, 4]);
